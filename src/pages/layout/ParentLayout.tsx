ff03098d11479e121f089bf951f93296e3f778cd
田中龍之介 <ry-tanaka@sios.com>
2024-02-23 10:07:03 +0900

    feature:designの追加

--- src/pages/layout/ParentLayout.tsx
+++ src/pages/layout/ParentLayout.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 
+import { FooterComponent } from '@/components/modules/FooterComponent';
+import { HeaderComponent } from '@/components/modules/HeaderComponent';
 import { UtilitiesConfig } from '@/utilities/UtilitiesConfig';
 
 export const ParentLayout = () => {
   return (
     <>
       <section>
+        <HeaderComponent />
         <main>
           <UtilitiesConfig>
             <Outlet />
           </UtilitiesConfig>
         </main>
+        <FooterComponent />
       </section>
     </>
   );
